45c7b1acfdb4e1aa23e7bdfcbec241c4e9178c03
Michael Haar <michael.haar@gmx.at>
2017-08-21 17:53:42 +0200

    I fixed a typo in the Configuration-Code and executed the file

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-Base = create_engine()
+Base = declarative_base()
 
 
 class Restaurant(Base):
