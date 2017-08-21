fb8a993b04380ac4d2cc79708a3fb75483481f16
Michael Haar <michael.haar@gmx.at>
2017-08-21 17:36:32 +0200

    Added the SQLAlchemy Mapper-Code
    This codepiece maps python objects to columns in our database.

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -10,8 +10,23 @@ class Restaurant(Base):
     # __tablename is a special variable that will be used to create a table
     __tablename__ = 'restaurant'
 
+    # map python objects to columns in our database
+    name = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key = True)
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+
+    # This is how you create relationships in SQLAlchemy:
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
 
 #################### insert at the end of the file ############################
 engine = create_engine('sqlite:///restaurantmenu.db')
