4a3d1873b3273ebce58ac2cffd79291fd2cafb51
Michael Haar <michael.haar@gmx.at>
2017-08-21 17:48:22 +0200

    Minor changes to make code PEP8 compliant

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -6,19 +6,21 @@ from sqlalchemy import create_engine
 
 Base = create_engine()
 
+
 class Restaurant(Base):
     # __tablename is a special variable that will be used to create a table
     __tablename__ = 'restaurant'
 
     # map python objects to columns in our database
-    name = Column(String(250), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     course = Column(String(250))
     description = Column(String(250))
     price = Column(String(8))
@@ -28,6 +30,6 @@ class MenuItem(Base):
     restaurant = relationship(Restaurant)
 
 
-#################### insert at the end of the file ############################
+# insert at the end of the file
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
