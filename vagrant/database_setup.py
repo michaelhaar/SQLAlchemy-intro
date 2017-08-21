31e290ea643cc2756aa027d68af4e0d9afac1541
Michael Haar <michael.haar@gmx.at>
2017-08-21 17:26:04 +0200

    Added the Class- and Table-Code for SQLAlchemy
    This code is a representation of a table as python class

--- vagrant/database_setup.py
+++ vagrant/database_setup.py
@@ -6,6 +6,12 @@ from sqlalchemy import create_engine
 
 Base = create_engine()
 
+class Restaurant(Base):
+    # __tablename is a special variable that will be used to create a table
+    __tablename__ = 'restaurant'
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
 
 #################### insert at the end of the file ############################
 engine = create_engine('sqlite:///restaurantmenu.db')
