0d5c60e453af32c73565911841d3e84bab308da9
Michael Haar <michael.haar@gmx.at>
2017-08-21 17:24:34 +0200

    Added the SQLAlchemy Configuration-Code
    Usually this code doesn't change much from project to project

---
+++ vagrant/database_setup.py
@@ -0,0 +1,12 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = create_engine()
+
+
+#################### insert at the end of the file ############################
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
